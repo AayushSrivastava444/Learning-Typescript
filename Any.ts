bff07a7215a949d42717a07a51b991e52ba1a33e
AayushSrivastava444 <Aayushsrivastava444@gmail.com>
2025-09-03 11:32:35 +0530

    Learned why we shouldn't use 'any' variable in ts

---
+++ Any.ts
@@ -0,0 +1,15 @@
+var hero;
+function myHero(){
+    return true;
+}
+ //This is a bad practice as we are using any
+hero=myHero()
+
+
+var hero : String
+function myHero(){
+    return true;
+}
+ //This will give false but this is good practice
+hero=myHero()
+
